198b16ab6b829121fdb4588e94e182644df3d928
Simon Lavers <simonlavers3@outlook.com>
2025-10-04 19:49:09 -0300

    Finished program

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
 import java.util.*;
 
-
 /**
  * Simon Lavers (#20479732).
  * CISC 124
  */
-
 
 
 public class Main {
@@ -18,18 +16,18 @@ public class Main {
         double startSeconds = seconds / 1000.0; //Start timer
         Date startDate = new Date(seconds); //Current date
 
-        List<List<Integer>> newMatrix = matrixTool.generateRandomMatrix(); //Create a new matrix
-
 
         int tries = 0;
 
         while(true) {
 
-            tries ++;
+            tries ++; //Count tries
 
             System.out.println(tries);
 
-            if(matrixTool.isSquareMatrix(newMatrix)) {
+            List<List<Integer>> newMatrix = matrixTool.generateRandomMatrix(); //Create a new matrix
+
+            if(matrixTool.isMagicMatrix(newMatrix)) {
 
                 System.out.println("It is a magic square");
                 System.out.println(newMatrix);
@@ -46,6 +44,7 @@ public class Main {
                 System.out.println("It started at :  " + startDate);
                 System.out.println("It ended   at :  " + endDate);
                 System.out.println("It took " + (endSeconds - startSeconds) + " seconds  to find the magic square");
+
                 break;
 
 
@@ -56,40 +55,44 @@ public class Main {
     }
 
 
-
-
-
-
-
 }
-
-
 
 class matrixTool {
 
 
     public static List<List<Integer>> generateRandomMatrix() {
 
+
+        /**
+         * This method is used to perform the following ability:
+         * 1: Generate a 3x3 matrix.
+         * 2: Returns a 3x3 matrix (Nested List)
+         */
+
+
         Random rand = new Random();
-        List<List<Integer>> magicMatrix = new ArrayList<>(); //Matrix
 
+        List<List<Integer>> magicMatrix = new ArrayList<>(3); //Matrix
 
-        int numOfTries = 0;
+        Set<Integer> used = new HashSet<>(9); //Store numbers that are already used to ensure no duplicates
+
 
         while (magicMatrix.size() < 3) {
 
-            List<Integer> row = new ArrayList<>();
+            List<Integer> row = new ArrayList<>(3); //Make row
 
-            for (int i = 0; i < 3; i++) {
+            while (row.size()<3){
 
                 int num = rand.nextInt(1, 10); //Assign Random Number
 
-                row.add(num);
+                if (used.add(num)) { //Returns false if there is duplicates
 
+                    row.add(num);
+                }
 
             }
 
-            magicMatrix.add(row);
+            magicMatrix.add(row); //Add row to column
 
         }
 
@@ -98,12 +101,52 @@ class matrixTool {
     }
 
 
-    public static boolean isSquareMatrix(List<List<Integer>> matrix) {
+    public static boolean isMagicMatrix(List<List<Integer>> matrix) {
 
-        return true;
+        /**
+         * This method is used to perform the following ability:
+         * 1: Perform the calculations to check if it is a magic square.
+         * 2: Returns a boolean if true.
+         */
 
 
+        if (matrix.isEmpty()) {
+
+
+            return false;
+
+        }
+
+        int isMagic = 15;
+
+        // rows and columns
+        for (int i = 0; i < 3; i++) {
+
+            int row = matrix.get(i).get(0) + matrix.get(i).get(1) + matrix.get(i).get(2); //Check row sum
+
+            int col = matrix.get(0).get(i) + matrix.get(1).get(i) + matrix.get(2).get(i); //Check column sum
+
+            if (row != isMagic || col != isMagic) {
+
+                return false;
+            }
+        }
+
+        // Now check diagonal values
+        int diagonalOne = matrix.get(0).get(0) + matrix.get(1).get(1) + matrix.get(2).get(2);
+
+        int diagonalTwo = matrix.get(0).get(2) + matrix.get(1).get(1) + matrix.get(2).get(0);
+
+        if (diagonalOne != isMagic || diagonalTwo != isMagic) {
+
+            return false;
+
+        }
+
+    return true;
     }
+
+
 
 
 }
